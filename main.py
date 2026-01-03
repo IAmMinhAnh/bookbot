89026898ca5cbbde5e5e5890fc1156271265c692
IAmMinhAnh <nguyendangminhanh8@gmail.com>
2026-01-03 12:25:04 -0500

    2: Completed project

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from stats import get_book_num_of_words, get_book_character_dictionary
+from stats import get_book_num_of_words, get_book_character_dictionary, sorting_dictionary
+import sys
+
+def report(book_link, book_num_of_words, dictionary):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_link}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_num_of_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in dictionary:
+        print(f"{char_dict["char"]}: {char_dict["num"]}")
+    print("============= END ===============")
 
 def get_book_text(book):
     with open(book) as f:
@@ -6,12 +17,17 @@ def get_book_text(book):
     return book_content
 
 def main():
-    book_link = "./books/frankenstein.txt"
+    if (len(sys.argv) != 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_link = sys.argv[1]
     book_content = get_book_text(book_link)
     book_num_of_words = get_book_num_of_words(book_content)
     book_chars_dictionary = get_book_character_dictionary(book_content)
-    print(f"Found {book_num_of_words} total words")
-    print(book_chars_dictionary)
+    dictionary = sorting_dictionary(book_chars_dictionary)
+
+    report(book_link, book_num_of_words, dictionary)
+    
 
 main()
     
--- stats.py
+++ stats.py
@@ -1,8 +1,23 @@
+def sorting_dictionary(dictionary):
+    def sort_on(items):
+        return items["num"]
+
+    list_of_chars = []
+    for char in dictionary:
+        temp_dict = {}
+        temp_dict["char"] = char
+        temp_dict["num"] = dictionary[char]
+        list_of_chars.append(temp_dict)
+
+    list_of_chars.sort(reverse=True, key=sort_on)
+    return list_of_chars
+
+
 def get_book_character_dictionary(content):
     content = content.lower()
     dictionary = {}
     for char in content:
-        if (char not in dictionary and char != " "):
+        if (char not in dictionary and char != " " and char.isalpha()):
             count = 0
             for sub_char in content:
                 if (sub_char == char):
